7e0553337df0d835cd9c388e30bafa08c78e147e
ragul28 <ragulh28@gmail.com>
2022-11-28 11:01:34 +0530

    Update the k8s version on install script

--- scripts/kubedm_install.sh
+++ scripts/kubedm_install.sh
@@ -1,10 +1,13 @@
 #/bin/bash
+# Get the latest release version https://kubernetes.io/releases/
+# Latest=1.25.4
+# Previous=1.24.8
 
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-sudo apt-get install -y kubelet=1.22.8-00 kubeadm=1.22.8-00 kubectl=1.22.8-00
+sudo apt-get install -y kubelet=1.24.8-00 kubeadm=1.24.8-00 kubectl=1.24.8-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
 sudo systemctl stop kubelet
